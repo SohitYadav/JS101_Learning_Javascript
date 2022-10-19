c74cc472792d233f80529b514be3499e7be59c5f
Sohit Yadav <beastgamerr57@gmail.com>
2022-10-19 10:27:27 +0530

    Logicalstatements

---
+++ Day3/prob2.js
@@ -0,0 +1,9 @@
+// Given any character, if it is a vowel print "Vowel"
+
+let char ="z";
+
+if(char=="a" || char=="e" || char=="i" || char=="o" || char=="u"){
+  console.log("Vowel");
+}else{
+  console.log("Consonant");
+}
---
+++ Day3/prob4.js
@@ -0,0 +1,15 @@
+
+// Given 3 numbers (all different values), print which is greatest
+
+let a=3;
+let b=4;
+let c=109;
+
+if(a>b && a>c){
+  console.log("a is the greatest");
+}else if(b>a && b>c){
+  console.log("b is the greatest");
+  
+}else if(c>a && c>b){
+  console.log("c is the greatest");
+}
